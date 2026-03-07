2e9e18afdb2cf4b6aeeed609101347b663fedab3
Peter Vanat <vanat.peter@gmail.com>
2026-03-07 13:47:14 +0100

    bumped version for real

--- version.php
+++ version.php
@@ -19,7 +19,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_qbankremotemanager';
-$plugin->version = 2025031301;
+$plugin->version = 2025031302;
 $plugin->requires = 2022041912;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.1';
