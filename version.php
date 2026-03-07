659ad30616b1bb798a08e4c2725cbc6cb9311e98
Peter Vanat <vanat.peter@gmail.com>
2026-03-07 16:47:45 +0100

    re-order

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_qbankremotemanager';
-$plugin->version = 2025031304;
+$plugin->version = 2025031305;
 $plugin->requires = 2022041912;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.3';
